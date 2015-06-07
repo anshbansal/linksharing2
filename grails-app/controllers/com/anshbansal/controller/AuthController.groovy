ae030b025fb4fd866392904bd098b2c4a6bf4057
Aseem Bansal <aseemb@intelligrape.com>
2015-06-07 13:43:51 +0530

    Updated AuthController to respond with JSON

--- grails-app/controllers/com/anshbansal/controller/AuthController.groovy
+++ grails-app/controllers/com/anshbansal/controller/AuthController.groovy
@@ -1,6 +1,14 @@
 package com.anshbansal.controller
 
+import com.anshbansal.dto.LoginCredentials
+
 class AuthController {
 
-    def index() { }
+    def authService
+
+    Boolean loginAuth(LoginCredentials credentials) {
+        render(contentType: 'application/json') {
+            isValid = authService.loginAuth(credentials)
+        }
+    }
 }
--- test/unit/com/anshbansal/controller/AuthControllerSpec.groovy
+++ test/unit/com/anshbansal/controller/AuthControllerSpec.groovy
@@ -1,11 +1,39 @@
 package com.anshbansal.controller
 
+import com.anshbansal.dto.LoginCredentials
+import com.anshbansal.service.AuthService
+
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
 @TestFor(AuthController)
 class AuthControllerSpec extends Specification {
 
-    void "test something"() {
+    void "test that isValid is sent as true for correct credentials"() {
+
+        setup: "Set authService to return true"
+        controller.authService = Mock(AuthService) {
+            1 * loginAuth(_ as LoginCredentials) >> true
+        }
+
+        when:
+        controller.loginAuth(new LoginCredentials())
+
+        then:
+        response.contentAsString == '{"isValid":true}'
+    }
+
+    void "test that isValid is sent as false for incorrect credentials"() {
+
+        setup: "Set authService to return true"
+        controller.authService = Mock(AuthService) {
+            1 * loginAuth(_ as LoginCredentials) >> false
+        }
+
+        when:
+        controller.loginAuth(new LoginCredentials())
+
+        then:
+        response.contentAsString == '{"isValid":false}'
     }
 }
