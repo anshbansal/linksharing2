02f4f72414ccdc6e6638e3815ef991d3df2f4b40
Aseem Bansal <aseemb@intelligrape.com>
2015-06-08 10:49:19 +0530

    Moved jquery and bootstrap to bower.json

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -70,9 +70,6 @@ grails.project.dependency.resolution = {
         // plugins needed at runtime but not for compilation
         runtime ":hibernate4:4.3.8.1" // or ":hibernate:3.6.10.18"
         runtime ":database-migration:1.4.0"
-        //TODO Move these to bower
-        runtime ":jquery:1.11.1"
-        runtime ':twitter-bootstrap:3.3.4'
 
         // Uncomment these to enable additional asset-pipeline capabilities
         //compile ":sass-asset-pipeline:1.9.0"
