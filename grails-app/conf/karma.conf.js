ea2cc8cb09da1783c5d01c4e847035e13377cefd
Aseem Bansal <aseemb@intelligrape.com>
2015-06-10 11:10:51 +0530

    Added coveralls for reporting

--- grails-app/conf/karma.conf.js
+++ grails-app/conf/karma.conf.js
@@ -36,10 +36,15 @@ module.exports = function (config) {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress', 'coverage'],
+        reporters: [
+            'progress',
+            'coverage',
+            'coveralls'
+        ],
 
         coverageReporter: {
-            type: 'text'
+            type: 'lcov',
+            dir: '../../logs/coverage/'
         },
 
         // web server port
